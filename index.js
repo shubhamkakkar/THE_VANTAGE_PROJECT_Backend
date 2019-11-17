e40c5f041b182aa13eee54f8f8118333c92d5d46
shubhamkakkar <shubham.kakkar998@gmail.com>
2019-11-17 21:58:07 +0530

    deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import {config} from "dotenv"
 import {resolve} from "path"
 import {ApolloServer} from 'apollo-server';
-import schema from './src/schema';
+import typeDefs from './src/schema';
 import resolvers from './src/resolvers'
 
 config({path: resolve(__dirname, "./.env")});
@@ -12,18 +12,12 @@ const MONGO_URI_DEV = process.env.MONGO_URI_DEV;
 
 mongoose.connect(MONGO_URI_DEV, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
     .then(_ => {
-        const server = new ApolloServer({typeDefs: schema, resolvers});
-
-        // const app = express();
-        // app.use(cors());
-        server.listen({port: process.env.PORT || 4000}).then(({url}) => {
-            console.log(`🚀 Server ready at ${url}`);
+        const server = new ApolloServer({typeDefs, resolvers});
+        server.listen({port: process.env.PORT || 2000}).then(({url}) => {
+            console.log(`🚀  Server ready at ${url}`);
         });
-        // server.applyMiddleware({app});
 
-        // app.listen({port}, () =>
-        //     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath} ${port}`)
-        // );
+
     })
     .catch((er) => console.log("failed to connect to mongoose", er));
 
